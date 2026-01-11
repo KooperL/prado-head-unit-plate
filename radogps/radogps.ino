f43767067298100c64d0401fd12ebf467d8b8d52
KooperL <kooper.lingohr@gmail.com>
2026-01-10 22:10:21 -0800

    grove gps working poc

--- radogps/radogps.ino
+++ radogps/radogps.ino
@@ -1,24 +1,42 @@
-#include <TFT_eSPI.h>
-#include <SPI.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
-TFT_eSPI tft = TFT_eSPI(); // TFT instance
+TinyGPSPlus gps;
+HardwareSerial GPSSerial(2);
+#define RX_PIN 33
+#define TX_PIN 32
+
+const long GPS_BAUD = 9600;
 
 void setup() {
   Serial.begin(115200);
-  tft.init();              // init display
-  tft.setRotation(1);      // set orientation
-  tft.fillScreen(TFT_BLACK);
+  Serial.println("Grove GPS starting at 9600 baud...");
 
-  tft.setTextSize(2);
-  tft.setTextColor(TFT_WHITE);
-  tft.setCursor(10, 10);
-  tft.print("Hello, T4 v1.3!");
+  GPSSerial.begin(GPS_BAUD, SERIAL_8N1, RX_PIN, TX_PIN);
 }
 
 void loop() {
-  // animate a simple rectangle
-  tft.fillRect(10, 50, 100, 20, TFT_BLUE);
-  delay(1000);
-  tft.fillRect(10, 50, 100, 20, TFT_BLACK);
-  delay(1000);
+  while (GPSSerial.available() > 0) {
+    char c = GPSSerial.read();
+    gps.encode(c);
+  }
+
+  if (gps.location.isUpdated() && gps.location.isValid()) {
+    Serial.print("Latitude: "); Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude: "); Serial.println(gps.location.lng(), 6);
+    Serial.print("Altitude: "); Serial.println(gps.altitude.meters());
+    Serial.print("Satellites: "); Serial.println(gps.satellites.value());
+    Serial.print("Date: "); Serial.print(gps.date.day()); Serial.print("/");
+    Serial.print(gps.date.month()); Serial.print("/");
+    Serial.println(gps.date.year());
+    Serial.print("Time: "); Serial.print(gps.time.hour()); Serial.print(":");
+    Serial.print(gps.time.minute()); Serial.print(":"); Serial.println(gps.time.second());
+
+    // Speed directly from GPS
+    Serial.print("Speed (km/h): "); Serial.println(gps.speed.kmph());
+    Serial.print("Speed (m/s): "); Serial.println(gps.speed.mps());
+    Serial.print("Speed (knots): "); Serial.println(gps.speed.knots());
+    
+    Serial.println("-------------------");
+  }
 }
